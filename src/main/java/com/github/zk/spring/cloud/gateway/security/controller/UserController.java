32dd5f420f0ae5979ba7237ce16363e8765422f4
zhaokai <506683059@qq.com>
2021-08-19 18:22:11 +0800

    新增POST请求获取用户信息

--- src/main/java/com/github/zk/spring/cloud/gateway/security/controller/UserController.java
+++ src/main/java/com/github/zk/spring/cloud/gateway/security/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -22,6 +23,20 @@ public class UserController {
 
     @GetMapping("/getUser")
     public Mono<Response> getUser() {
+        return ReactiveSecurityContextHolder.getContext()
+                .switchIfEmpty(Mono.error(new IllegalStateException("ReactiveSecurityContext is empty")))
+                .map(SecurityContext::getAuthentication)
+                .map(Authentication::getPrincipal)
+                .cast(UserInfo.class)
+                .map(userInfo -> {
+                    Response response = Response.getInstance();
+                    response.setOk(Response.CodeEnum.SUCCESSED, null, "查询成功！", userInfo);
+                    return response;
+                });
+    }
+
+    @PostMapping("queryUser")
+    public Mono<Response> queryUser() {
         return ReactiveSecurityContextHolder.getContext()
                 .switchIfEmpty(Mono.error(new IllegalStateException("ReactiveSecurityContext is empty")))
                 .map(SecurityContext::getAuthentication)
