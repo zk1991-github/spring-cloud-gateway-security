4d1b9a432338dec1d7489ead93a76e3824e551a3
zhaokai <506683059@qq.com>
2021-12-03 18:23:36 +0800

    新增登录处理器类

---
+++ src/main/java/com/github/zk/spring/cloud/gateway/security/core/LoginProcessor.java
@@ -0,0 +1,168 @@
+package com.github.zk.spring.cloud.gateway.security.core;
+
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.security.web.authentication.session.SessionAuthenticationException;
+import org.springframework.session.data.redis.ReactiveRedisSessionRepository;
+import org.springframework.web.server.WebSession;
+import reactor.core.publisher.Mono;
+
+/**
+ * 基础登录
+ *
+ * @author zk
+ * @date 2021/11/30 15:06
+ */
+public class LoginProcessor {
+
+    private final ReactiveStringRedisTemplate reactiveStringRedisTemplate;
+
+    private final ReactiveRedisSessionRepository sessionRepository;
+
+    /**
+     * 同时在线最大用户数
+     */
+    private int maxSessions;
+
+    public LoginProcessor(ReactiveStringRedisTemplate reactiveStringRedisTemplate,
+                          ReactiveRedisSessionRepository sessionRepository) {
+        this.reactiveStringRedisTemplate = reactiveStringRedisTemplate;
+        this.sessionRepository = sessionRepository;
+    }
+
+    /**
+     * 设置同时在线最大用户数
+     *
+     * @param maxSessions 同时在线最大用户数
+     */
+    public void setMaxSessions(int maxSessions) {
+        this.maxSessions = maxSessions;
+    }
+
+    /**
+     * web会话处理
+     *
+     * @param hashKey    用户缓存key
+     * @param webSession web会话
+     * @return
+     */
+    public Mono<Boolean> webSessionProcess(String hashKey, WebSession webSession) {
+        return getSessionId(hashKey)
+                .flatMap(sessionId -> changeSession(hashKey, sessionId, webSession.getId()))
+                .switchIfEmpty(Mono.defer(() -> saveSession(hashKey, webSession.getId())));
+    }
+
+    /**
+     * 会话限制处理
+     * 已经登录过的用户，因异地登录或重复登录时，不受最大登录用户数限制。
+     * 流程如下：
+     * 1. 获取用户 SessionId
+     * 2. 判断用户 Session 是否存在
+     * 如果存在：返回用户信息
+     * 如果不存在：判断在线用户数是否超过允许登录最大用户数
+     * 如果超过：返回错误信息
+     * 如果未超过：返回用户信息
+     *
+     * @param hashKey 缓存key
+     * @return
+     */
+    public Mono<Boolean> sessionLimitProcess(String hashKey) {
+        //判断是否超过允许最大登录人数
+        return getSessionId(hashKey)
+                .flatMap(this::userSessionExistBySessionId)
+                .switchIfEmpty(onlineNum()
+                        .map(aLong -> maxSessions != -1 && aLong >= maxSessions)
+                        .flatMap(aBoolean -> {
+                            if (aBoolean) {
+                                return Mono.defer(() -> Mono.error(new SessionAuthenticationException("超过最大登录人数")));
+                            } else {
+                                return Mono.just(true);
+                            }
+                        }));
+    }
+
+    /**
+     * 获取会话id
+     * 用于判断当前用户是否登录
+     *
+     * @param hashKey 用户缓存key
+     * @return
+     */
+    public Mono<String> getSessionId(String hashKey) {
+        return reactiveStringRedisTemplate
+                .opsForHash()
+                .get("sessions", hashKey)
+                .switchIfEmpty(Mono.empty())
+                .cast(String.class);
+    }
+
+    /**
+     * 改变登录会话，删除老会话，保存新会话(异地踢出)
+     *
+     * @param hashKey      用户缓存key
+     * @param oldSessionId 老会话id
+     * @param newSessionId 新会话id
+     */
+    public Mono<Boolean> changeSession(String hashKey, String oldSessionId, String newSessionId) {
+        Mono<Void> delSession = sessionRepository.deleteById(oldSessionId);
+        Mono<Boolean> removeSession = removeSession(hashKey);
+        Mono<Boolean> saveSession = saveSession(hashKey, newSessionId);
+        return delSession.then(removeSession).then(saveSession);
+    }
+
+    /**
+     * 保存会话
+     *
+     * @param hashKey   用户缓存key
+     * @param sessionId 会话id
+     */
+    public Mono<Boolean> saveSession(String hashKey, String sessionId) {
+        return reactiveStringRedisTemplate
+                .opsForHash()
+                .put("sessions", hashKey, sessionId);
+    }
+
+    /**
+     * 删除会话
+     *
+     * @param hashKey 用户缓存key
+     * @return
+     */
+    public Mono<Boolean> removeSession(String hashKey) {
+        return reactiveStringRedisTemplate
+                .opsForHash()
+                .remove("sessions", hashKey).map(delNum -> delNum > 0);
+    }
+
+    /**
+     * 在线用户数
+     *
+     * @return
+     */
+    public Mono<Long> onlineNum() {
+        ScanOptions options = ScanOptions
+                .scanOptions()
+                .match(ReactiveRedisSessionRepository.DEFAULT_NAMESPACE + ":*")
+                .build();
+        return reactiveStringRedisTemplate.scan(options).count();
+    }
+
+    /**
+     * 根据 SessionId， 查询用户 Session 是否存在
+     *
+     * @param sessionId
+     * @return
+     */
+    public Mono<Boolean> userSessionExistBySessionId(String sessionId) {
+        ScanOptions options = ScanOptions
+                .scanOptions()
+                .match(ReactiveRedisSessionRepository.DEFAULT_NAMESPACE + ":sessions:" + sessionId)
+                .build();
+        return reactiveStringRedisTemplate.scan(options).count().flatMap(count -> {
+            if (count > 0) {
+                return Mono.just(true);
+            }
+            return Mono.empty();
+        });
+    }
+}
