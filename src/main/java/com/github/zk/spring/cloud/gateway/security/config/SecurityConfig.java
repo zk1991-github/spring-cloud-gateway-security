0f5c8bc161447d5116033b3bb7b48502a1292a92
zhaokai <506683059@qq.com>
2021-12-03 18:29:40 +0800

    新增微信认证鉴权功能

--- src/main/java/com/github/zk/spring/cloud/gateway/security/config/SecurityConfig.java
+++ src/main/java/com/github/zk/spring/cloud/gateway/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.github.zk.spring.cloud.gateway.security.config;
 
+import com.github.zk.spring.cloud.gateway.security.core.LoginProcessor;
 import com.github.zk.spring.cloud.gateway.security.handler.CustomReactiveAuthenticationManager;
 import com.github.zk.spring.cloud.gateway.security.handler.CustomReactiveAuthorizationManager;
 import com.github.zk.spring.cloud.gateway.security.handler.CustomRedirectServerAuthenticationFailureHandler;
@@ -17,6 +18,7 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationEntryPoint;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
+import org.springframework.session.data.redis.ReactiveRedisSessionRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
@@ -53,17 +55,21 @@ public class SecurityConfig {
      */
     private final String INVALID_URL = "/login/invalid";
 
+    private final String WECHAT_URL = "/login/weChatLogin";
+
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, GatewayProperties gatewayProperties) {
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
+                                                            GatewayProperties gatewayProperties,
+                                                            LoginProcessor loginProcessor) {
         http
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers(SUCCESS_URL, FAIL_URL, INVALID_URL).permitAll()
+                        .pathMatchers(SUCCESS_URL, FAIL_URL, INVALID_URL, WECHAT_URL).permitAll()
                         .anyExchange()
                         .access(new CustomReactiveAuthorizationManager(gatewayProperties))
                 )
                 .httpBasic().disable()
                 .formLogin()
-                .authenticationManager(new CustomReactiveAuthenticationManager(defaultUserImpl(), maxSessions, reactiveStringRedisTemplate))
+                .authenticationManager(new CustomReactiveAuthenticationManager(defaultUserImpl(), loginProcessor))
                 //登录服务地址
                 .loginPage(LOGIN_URL)
                 .authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler(
@@ -78,6 +84,14 @@ public class SecurityConfig {
 //                .exceptionHandling().accessDeniedHandler(new HttpStatusServerAccessDeniedHandler(HttpStatus.FORBIDDEN))
         ;
         return http.build();
+    }
+
+    @Bean
+    public LoginProcessor loginProcessor(ReactiveStringRedisTemplate reactiveStringRedisTemplate,
+                                         ReactiveRedisSessionRepository sessionRepository) {
+        LoginProcessor loginProcessor = new LoginProcessor(reactiveStringRedisTemplate, sessionRepository);
+        loginProcessor.setMaxSessions(maxSessions);
+        return loginProcessor;
     }
 
     @Bean
