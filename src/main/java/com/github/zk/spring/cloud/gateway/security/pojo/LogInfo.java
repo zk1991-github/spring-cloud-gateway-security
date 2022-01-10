35c0db5e4ddb18647c55fecb36388d77edcfd84f
zhaokai <506683059@qq.com>
2022-01-10 15:33:05 +0800

    日志信息实体

---
+++ src/main/java/com/github/zk/spring/cloud/gateway/security/pojo/LogInfo.java
@@ -0,0 +1,98 @@
+package com.github.zk.spring.cloud.gateway.security.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+/**
+ * 日志实体类
+ *
+ * @author zk
+ * @date 2022/1/6 15:06
+ */
+@TableName("t_log")
+public class LogInfo {
+    /** 唯一id */
+    @TableId(type = IdType.ASSIGN_ID)
+    private String id;
+    /** 用户id */
+    private String userId;
+    /** 用户名 */
+    private String username;
+    /** ip地址 */
+    private String ip;
+    /** 登录状态 */
+    private Integer status;
+    /** 登录信息 */
+    private String msg;
+    /** 登录时间 */
+    private String time;
+
+    public LogInfo() {
+    }
+
+    public LogInfo(String userId, String username, String ip, Integer status, String msg, String time) {
+        this.userId = userId;
+        this.username = username;
+        this.ip = ip;
+        this.status = status;
+        this.msg = msg;
+        this.time = time;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
