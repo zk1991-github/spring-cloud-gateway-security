0ae1992e0aba53e8d661918b0b1cd5613148a0d3
zhaokai <506683059@qq.com>
2022-01-10 15:32:10 +0800

    日志公共方法抽取

---
+++ src/main/java/com/github/zk/spring/cloud/gateway/security/log/LogHolder.java
@@ -0,0 +1,73 @@
+package com.github.zk.spring.cloud.gateway.security.log;
+
+import com.github.zk.spring.cloud.gateway.security.pojo.LogInfo;
+import com.github.zk.spring.cloud.gateway.security.pojo.UserInfo;
+import com.github.zk.spring.cloud.gateway.security.pojo.WeChatUserInfo;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+import org.springframework.web.server.ServerWebExchange;
+
+/**
+ * 日志持有类
+ *
+ * @author zk
+ * @date 2022/1/6 9:24
+ */
+public class LogHolder {
+
+    /**
+     * 日志实现方式
+     */
+    private Log log = new ControlLog();
+
+    /**
+     * 日志记录开关
+     */
+    private boolean enabled;
+
+    public Log getLog() {
+        return log;
+    }
+
+    public void setLog(Log log) {
+        this.log = log;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    /**
+     * 登录日志
+     * @param exchange 请求
+     * @param user 用户信息
+     * @param status 状态
+     * @param msg 消息
+     */
+    public void loginLog(ServerWebExchange exchange, Object user, int status, String msg) {
+        if (!this.enabled) {
+            return;
+        }
+        String userId = "";
+        String username = "";
+        if (user instanceof UserInfo) {
+            UserInfo userInfo = (UserInfo) user;
+            userId = String.valueOf(userInfo.getId());
+            username = userInfo.getUsername();
+        } else {
+            WeChatUserInfo weChatUserInfo = (WeChatUserInfo) user;
+            userId = weChatUserInfo.getOpenid();
+            username = weChatUserInfo.getNickName();
+        }
+        String ip = Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getHostName();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String time = LocalDateTime.now().format(dateTimeFormatter);
+        LogInfo logInfo = new LogInfo(userId, username, ip, status, msg, time);
+        log.loginLog(logInfo);
+    }
+}
