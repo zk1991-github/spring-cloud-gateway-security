c8777657e99f153eff13845f7f392694b933be04
zhaokai <506683059@qq.com>
2022-02-17 09:53:45 +0800

    创建角色权限实体

---
+++ src/main/java/com/github/zk/spring/cloud/gateway/security/pojo/RolePermission.java
@@ -0,0 +1,64 @@
+/*
+ *
+ *  * Copyright 2021-2022 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      https://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.github.zk.spring.cloud.gateway.security.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+/**
+ * 角色权限关联实体
+ *
+ * @author zk
+ * @date 2022/2/15 14:45
+ */
+@TableName("role_permission")
+public class RolePermission {
+    /** 唯一id */
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+    /** 角色id */
+    private Long roleId;
+    /** 权限id */
+    private Long permissionId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public Long getPermissionId() {
+        return permissionId;
+    }
+
+    public void setPermissionId(Long permissionId) {
+        this.permissionId = permissionId;
+    }
+}
