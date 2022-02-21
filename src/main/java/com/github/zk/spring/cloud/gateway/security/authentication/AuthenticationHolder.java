9faf08ea125c59a075aaccb8ed9d41256a792fe5
zhaokai <506683059@qq.com>
2022-02-21 13:31:33 +0800

    创建认证持有实体

---
+++ src/main/java/com/github/zk/spring/cloud/gateway/security/authentication/AuthenticationHolder.java
@@ -0,0 +1,51 @@
+/*
+ *
+ *  * Copyright 2021-2022 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      https://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.github.zk.spring.cloud.gateway.security.authentication;
+
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.core.Authentication;
+
+/**
+ * 权限持有类
+ *
+ * @author zk
+ * @date 2022/2/21 10:53
+ */
+public class AuthenticationHolder {
+    /** 认证 */
+    private Authentication authentication;
+    /** 权限 */
+    private AuthorizationDecision authorizationDecision;
+
+    public Authentication getAuthentication() {
+        return authentication;
+    }
+
+    public void setAuthentication(Authentication authentication) {
+        this.authentication = authentication;
+    }
+
+    public AuthorizationDecision getAuthorizationDecision() {
+        return authorizationDecision;
+    }
+
+    public void setAuthorizationDecision(AuthorizationDecision authorizationDecision) {
+        this.authorizationDecision = authorizationDecision;
+    }
+}
