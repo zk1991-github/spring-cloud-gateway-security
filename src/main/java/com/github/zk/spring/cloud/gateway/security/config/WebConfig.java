03fde7de1ebd59f702a0c6dd8251e98f1d1c5658
zhaokai <506683059@qq.com>
2023-03-29 17:44:33 +0800

    增加编码器配置，解决前端js的Long类型精度丢失问题

---
+++ src/main/java/com/github/zk/spring/cloud/gateway/security/config/WebConfig.java
@@ -0,0 +1,50 @@
+/*
+ *
+ *  * Copyright 2021-2022 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      https://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.github.zk.spring.cloud.gateway.security.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.codec.Encoder;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+/**
+ * web 配置， 解决前端Long类型丢精度问题
+ *
+ * @author zk
+ * @date 2023/3/28 17:09
+ */
+@Configuration
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().build();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(module);
+        Encoder<?> encoder = new Jackson2JsonEncoder(objectMapper);
+        configurer.defaultCodecs().jackson2JsonEncoder(encoder);
+    }
+}
