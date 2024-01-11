c95666c5e3c64ec500de47836870fb313377c940
zhaokai <506683059@qq.com>
2024-01-11 18:15:49 +0800

    新增生成密码加密接口

---
+++ src/main/java/com/github/zk/spring/cloud/gateway/security/controller/GeneratorController.java
@@ -0,0 +1,51 @@
+/*
+ *
+ *  * Copyright 2021-2024 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      https://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.github.zk.spring.cloud.gateway.security.controller;
+
+import com.github.zk.spring.cloud.gateway.security.common.Response;
+import com.github.zk.spring.cloud.gateway.security.util.PasswordGeneratorUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 生成器 请求控制
+ *
+ * @author zk
+ * @date 2023/12/25 10:27
+ */
+@RestController
+@RequestMapping("/gateway")
+public class GeneratorController {
+
+    @GetMapping("/passwordGenerator")
+    public Response passwordGenerator(@RequestParam("password") String password) {
+        Response response = Response.getInstance();
+        if (ObjectUtils.isEmpty(password)) {
+            response.setError(Response.CodeEnum.FAIL, "/generator/password", "生成失败！");
+        } else {
+            String encodePassword = PasswordGeneratorUtils.encodePassword(password);
+            response.setOk(Response.CodeEnum.SUCCESSED, "/generator/password", "生成成功！", encodePassword);
+        }
+
+        return response;
+    }
+}
