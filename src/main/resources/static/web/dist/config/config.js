c92c54d0f1aacf5e22a32a090c9d617f9d8605d8
zhaokai <506683059@qq.com>
2025-03-31 18:09:49 +0800

    增加分组功能

--- src/main/resources/static/web/dist/config/config.js
+++ src/main/resources/static/web/dist/config/config.js
@@ -6,6 +6,7 @@
  * @FilePath: \GatewayWeb\public\config\config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+///gatewayservice
 let GATEWAY_URL = "/gatewayservice";
 let strArr = document.location.pathname.match(new RegExp("\\w*(?=/web)", "g"));
 if (strArr && strArr.length) {
@@ -19,6 +20,6 @@ if (strArr && strArr.length) {
 }
 
 var $url = {
-    // DATA_URL: GATEWAY_URL,
-    DATA_URL: "http://" + window.location.host + GATEWAY_URL,
+    //DATA_URL: GATEWAY_URL,
+     DATA_URL: "http://" + window.location.host + GATEWAY_URL,
 };
